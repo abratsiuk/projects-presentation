312820b0af6da91b2a8c5a76f2b6c2e38c1b356d
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-05-26 11:32:39 +0300

    add Angular in position, cv, presentation

--- src/services/projectsDataEn.ts
+++ src/services/projectsDataEn.ts
@@ -12,13 +12,13 @@ export const projectsData: IProject[] = [
     images: [],
     nameShort: 'ngrx-shop',
     name: 'NgRx store',
-    shortDescription:
-      'NgRx store (the same as Redux store, but in Angular and NgRx). State: started.',
+    shortDescription: 'NgRx store',
     fullDescription: [
       'Store built with Angular and NgRx ',
       'Store based on the API from the game fortniteapi.io',
       'Purchases from the product catalog go to the cart. After confirmation, the cart becomes an order. There is an order list. An order can be accepted or canceled. If canceled, the funds are refunded. The cart and order history are stored in LocalStorage.',
       'Funds can be added on the profile page.',
+      '// It’s the same as a Redux store, but implemented in Angular with NgRx.',
     ],
     technology: [
       { image: 'images/technology/angular32.png', name: 'Angular' },
