80ad4049888405ec280c962cc25e3ab20f51d935
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-06-12 09:30:53 +0300

    change cv

--- src/services/personData.ts
+++ src/services/personData.ts
@@ -3,9 +3,9 @@ import { IPerson } from '../interfaces/IPerson';
 export const personData: IPerson = {
   photo: 'images/person/person.webp',
   name: 'Александр Брацюк',
-  position: 'React Developer',
+  position: 'Frontend Developer (React)',
   email: 'bratsiuk.job@outlook.com',
-  skype: 'live:.cid.ab5d1bb1e381a0c3',
+  skype: '',
   cv: 'cv/cv_Aleksandr_Bratsiuk.pdf',
   repo: 'https://github.com/abratsiuk',
 };
--- src/services/personDataEn.ts
+++ src/services/personDataEn.ts
@@ -3,9 +3,9 @@ import { IPerson } from '../interfaces/IPerson';
 export const personData: IPerson = {
   photo: 'images/person/person.webp',
   name: 'Aleksandr Bratsiuk',
-  position: 'full stack NET/ React, Angular developer',
+  position: 'Frontend Developer (React)',
   email: 'bratsiuk.job@outlook.com',
-  skype: 'live:.cid.ab5d1bb1e381a0c3',
+  skype: '',
   cv: 'cv/cv_Aleksandr_Bratsiuk.pdf',
   repo: 'https://github.com/abratsiuk',
 };
