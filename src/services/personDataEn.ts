8911d4d044b38f98ddd45c62ecec6fb55129691c
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-06-12 10:43:54 +0300

    change cv

--- src/services/personDataEn.ts
+++ src/services/personDataEn.ts
@@ -3,7 +3,7 @@ import { IPerson } from '../interfaces/IPerson';
 export const personData: IPerson = {
   photo: 'images/person/person.webp',
   name: 'Aleksandr Bratsiuk',
-  position: 'Frontend Developer (React)',
+  position: 'Frontend Developer (React) / Full-Stack Developer (.NET/React)',
   email: 'bratsiuk.job@outlook.com',
   skype: '',
   cv: 'cv/cv_Aleksandr_Bratsiuk.pdf',
