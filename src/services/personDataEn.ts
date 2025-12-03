0a753e3c79831a031af81a94ed6f8736e7914119
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-12-03 17:48:06 +0200

    ...

--- src/services/personDataEn.ts
+++ src/services/personDataEn.ts
@@ -3,9 +3,9 @@ import { IPerson } from '../interfaces/IPerson';
 export const personData: IPerson = {
   photo: 'images/person/person.webp',
   name: 'Oleksandr Bratsiuk',
-  position: 'Senior .NET Fullstack Engineer (Backend-focused)',
+  position: 'Senior .NET Full-Stack Engineer (Backend-focused)',
   email: 'bratsiuk.job@outlook.com',
   skype: '',
-  cv: 'cv/Oleksandr_Bratsiuk_Senior_NET_Fullstack_Engineer_2025.pdf',
+  cv: 'cv/Oleksandr_Bratsiuk_Senior_NET_Full-Stack_Engineer_2025.pdf',
   repo: 'https://github.com/abratsiuk',
 };
